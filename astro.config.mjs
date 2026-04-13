2be9061e599a3e5b5ff1f3b3a8701ec134ae5c1e
Salim Mahboubi <ztickm@gmail.com>
2026-04-13 22:07:00 +0200

    chore: replace Jekyll .gitignore with Astro .gitignore

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://www.smhb.me",
+  integrations: [mdx(), sitemap(), tailwind()],
+});
